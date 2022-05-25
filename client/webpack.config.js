55cb483f4522e3df1b835757e13924d7b7afef04
atmason90 <atmason90@gmail.com>
2022-05-25 10:38:11 -0700

    add output, plugins and loaders to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -11,17 +14,57 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+     new HtmlWebpackPlugin({
+       template: './index.html',
+       title: 'Contact Directory'
+     }),
+     new InjectManifest({
+       swSrc: './src-sw.js',
+       swDest: 'src-sw.js'
+     }),
+     new WebpackPwaManifest({
+       name: 'Contact Directory',
+       short_name: 'Contacts',
+       description: 'A way to manage all of your contacts',
+       background_color: '#272822',
+       theme_color: '#272822',
+       start_url: '/',
+       publicPath: '/',
+       icons: [
+         {
+           src: path.resolve('src/images/logo.png'),
+           size: [96, 128, 192, 256, 384, 512],
+           destination: path.join('assets', 'icons'),
+         },
+       ],
+     }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        },
+      ],
     }
   };
 };
