39a0f6c65487732fedeff433babe74436ecaaf48
atmason90 <atmason90@gmail.com>
2022-05-25 11:26:16 -0700

    finalize files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,23 +3,51 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+    openDB('contact', 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains('contact')) {
+                console.log('contact database already exists');
+                return;
+            }
+            db.createObjectStore('contact', { keyPath: 'id', autoIncrement: true });
+            console.log('contact database created');
+        }
+    })
 };
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+    console.log('POST to database');
+    const contactDb = await openDB('contact', 1);
+    const tx = contactDb.transaction('contact', 'readwrite');
+    const store = tx.objectStore('contact');
+    const request = store.post({ name: name, home: home, cell: cell, email: email });
+    const result = await request;
+    console.log('data saved to database', result);
 };
 
 // TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+  console.log('GET all data from database');
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 // TODO: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+  console.log('DELETE database');
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readwrite');
+  const store = tx.objectStore('contact');
+  const request = store.delete();
+  const result = await request;
+  console.log('database deleted', result);
 };
 
 initdb();
